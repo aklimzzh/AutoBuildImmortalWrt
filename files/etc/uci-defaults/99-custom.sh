4dd6437be93922112b257230a565b7b234865059
Zach ZHOU <10203540+aklimzzh@users.noreply.github.com>
2025-01-08 17:11:50 +0800

    Update 99-custom.sh
    update ip addr config

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -21,9 +21,9 @@ done
 
 # 网络设置
 if [ "$count" -eq 1 ]; then
-  uci set network.lan.proto='dhcp'
+  uci set network.lan.ipaddr='192.168.123.2'
 elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ipaddr='192.168.100.1'
+  uci set network.lan.ipaddr='192.168.123.1'
 fi
 
 # 设置所有网口可访问网页终端
